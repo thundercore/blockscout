c96fa8ac73bf68f4df594165f7a209e61243d7fc
waynetseng <wayne.tseng@thundercore.tw>
2021-12-06 16:51:35 +0800

    Remove meaningless dropdown

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -37,7 +37,7 @@ defmodule BlockScoutWeb.LayoutView do
   end
 
   def subnetwork_title do
-    Keyword.get(application_config(), :subnetwork) || "ThunderCore Test"
+    Keyword.get(application_config(), :subnetwork) || "ThunderCore Testnet"
   end
 
   def network_title do
